d11aa5cef0d8e7df05a21efb9365fd1b6f4b3e05
Wajeeh <alamwajeeh@gmail.com>
2025-03-08 15:43:42 -0500

    heklp

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { IoCopyOutline } from 'react-icons/io5';
-import Lottie from 'react-lottie';
-import { cn } from '@/lib/utils';
+import dynamic from 'next/dynamic';
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });import { cn } from '@/lib/utils';
 // import BackgroundGradientAnimation from "./GradientBg"
 import animationData from '@/data/confetti.json';
 import MagicButton from '../MagicButton';
