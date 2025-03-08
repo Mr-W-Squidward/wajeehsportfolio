a0b58b064b703477eb053db9773b8523faf0b057
Wajeeh <alamwajeeh@gmail.com>
2025-03-08 13:19:32 -0500

    hi

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -42,7 +42,7 @@ const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
-    if (typeof window === "undefined") return;
+    if (typeof document === "undefined") return;
 
     document.body.style.setProperty(
       "--gradient-background-start",
